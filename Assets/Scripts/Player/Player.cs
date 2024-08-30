ab88417b152799b44416b3998a26b9f356b017f9
CleytonRam <cleytonglauco@gmail.com>
2024-08-30 18:17:23 -0400

    implement first prot of player walking

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Rigidbody2D myRigidbody;
+
+    public Vector2 velocity;
+   
+    public float speed;
+
+    public void Update()
+    {
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+
+            myRigidbody.velocity = new Vector2(-speed, myRigidbody.velocity.y);
+
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+
+            myRigidbody.velocity = new Vector2(speed, myRigidbody.velocity.y);
+
+        }
+    }
+}
